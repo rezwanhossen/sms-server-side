914284fb895d69c5f8c45ff214fa3d98de6a77bc
rezwan-hossen <rezwanhossen625@gmail.com>
2024-06-07 22:07:36 +0600

    verify token and admin verifi user add role patch etc.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+//const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 // middleware
@@ -12,29 +12,12 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.json());
 
-//======================================================
-// Verify Token Middleware
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token;
-  console.log(token);
-  if (!token) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      console.log(err);
-      return res.status(401).send({ message: "unauthorized access" });
-    }
-    req.user = decoded;
-    next();
-  });
-};
 //=============================================================================
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.p5jkrsj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -49,38 +32,119 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const mealcolection = client.db("hostalDB").collection("meals");
+    const usercol = client.db("hostalDB").collection("users");
     // auth related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "365d",
+        expiresIn: "5h",
       });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        })
-        .send({ success: true });
+      res.send({ token });
     });
     // Logout
-    app.get("/logout", async (req, res) => {
-      try {
-        res
-          .clearCookie("token", {
-            maxAge: 0,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-          })
-          .send({ success: true });
-        console.log("Logout successful");
-      } catch (err) {
-        res.status(500).send(err);
+    // app.get("/logout", async (req, res) => {
+    //   try {
+    //     res
+    //       .clearCookie("token", {
+    //         maxAge: 0,
+    //         secure: process.env.NODE_ENV === "production",
+    //         sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    //       })
+    //       .send({ success: true });
+    //   } catch (err) {
+    //     res.status(500).send(err);
+    //   }
+    // });
+
+    //======================================================
+    // Verify Token Middleware
+    const verifyToken = async (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
       }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+    //veryfy admin
+    const veryfiAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usercol.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbennen akcess" });
+      }
+      next();
+    };
+
+    //==========================user collection=================================
+
+    app.get("/users", verifyToken, async (req, res) => {
+      const result = await usercol.find().toArray();
+      res.send(result);
     });
+
+    app.get("/user/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorize" });
+      }
+      const query = { email: email };
+      const user = await usercol.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
+    app.get("/useron/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usercol.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usercol.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    //save a user
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email };
+      // cheack if user alrady
+      const isExist = await usercol.findOne(query);
+      if (isExist) return res.send(isExist);
+
+      const result = await usercol.insertOne(user);
+      res.send(result);
+    });
+
     //==========================meals===========================================
     app.get("/meals", async (req, res) => {
       const result = await mealcolection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/meal/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await mealcolection.findOne(query);
       res.send(result);
     });
 
@@ -100,5 +164,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`StayVista is running on port ${port}`);
+  console.log(`sms is running on port ${port}`);
 });
